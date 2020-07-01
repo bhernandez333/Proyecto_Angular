39599bcac7d46eadb570aab374c9d3194e61d7ba
bhernandez333 <61214858+bhernandez333@users.noreply.github.com>
2020-06-30 20:53:51 -0600

    Actualización de lista e item.
    Actualización de lista e item: se actualiza lista, item, tarea y otros.

---
+++ src/app/models/tarea.ts
@@ -0,0 +1,8 @@
+export class Tarea {
+    // id, descripcion, fecha, usuario, estado.
+    public id: string;
+    public descripcion: string;
+    public fecha: Date;
+    public usuario: string;
+    public estado: boolean;
+}
